61e054c6588fd08ce668a22241447fd982e78cc1
lesguillemets <lesguillemets@users.noreply.github.com>
2015-02-03 00:35:26 +0900

    add total sum for each line

--- Display.hs
+++ Display.hs
@@ -7,8 +7,12 @@ import GHC.Exts (groupWith)
 display :: Array (Int,Int) Double -> String
 display arr = let
     lns = map sort . groupWith fst . indices $ arr
+    showLn = unwords . map toFixedDigits . addLineSummary . map (arr !)
     in
-        unlines . map (unwords . map (toFixedDigits . (arr !))) $ lns
+        unlines . map showLn $ lns
+
+addLineSummary :: [Double] -> [Double]
+addLineSummary ln = ln ++ [sum ln / fromIntegral (length ln)]
 
 showPlayers :: [FilePath] -> String
 showPlayers = let titles = map (printf "%2d.\t") ([1..] :: [Int])
