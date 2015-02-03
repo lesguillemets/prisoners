e23f1c8115e026ef48d9ddec1f5a9b452a8dbf72
lesguillemets <lesguillemets@users.noreply.github.com>
2015-02-03 11:53:10 +0900

    better table display

--- Display.hs
+++ Display.hs
@@ -7,16 +7,20 @@ import GHC.Exts (groupWith)
 display :: Array (Int,Int) Double -> String
 display arr = let
     lns = map sort . groupWith fst . indices $ arr
-    showLn = unwords . map toFixedDigits . addLineSummary . map (arr !)
+    showLn = unwords . map showCell . addLineSummary . map (arr !)
     in
-        unlines . map showLn $ lns
+        unlines . addPlayerInfo . map showLn $ lns
 
 addLineSummary :: [Double] -> [Double]
 addLineSummary ln = ln ++ [sum ln / fromIntegral (length ln)]
+
+addPlayerInfo :: [String] -> [String]
+addPlayerInfo = let p = printf "%2d || " :: Int -> String in
+        zipWith (++) (map p [1..])
 
 showPlayers :: [FilePath] -> String
 showPlayers = let titles = map (printf "%2d.\t") ([1..] :: [Int])
                   in unlines . zipWith (++) titles
 
-toFixedDigits :: Double -> String
-toFixedDigits = printf "%.4f |"
+showCell :: Double -> String
+showCell = printf "%.4f |"
